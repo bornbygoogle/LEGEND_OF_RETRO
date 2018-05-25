1b5ebf936cd20ce2ff1ad21f2c7f700146e7dd7d
iulian1976 <iulian_bulan@yahoo.com>
2018-05-26 00:27:41 +0200

    My initial commit message

---
+++ LegendOfRetro/src/controleur/DonneesInsuffisantesException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controleur;
+
+/**
+ *
+ * @author Home
+ */
+public class DonneesInsuffisantesException extends Exception
+{
+    public DonneesInsuffisantesException(String erreur)
+    {
+        super(erreur);
+    }
+}
