ca4f5e061c853b156666b6e64a10eeab869b8aee
iulian1976 <iulian_bulan@yahoo.com>
2018-05-31 20:47:20 +0200

    Le menu Achat hérite du menu Vente.

--- LegendOfRetro/src/vue/menuAchat.java
+++ LegendOfRetro/src/vue/menuAchat.java
@@ -23,11 +23,10 @@ import javax.swing.JPanel;
  *
  * @author Home
  */
-public class menuAchat extends JPanel implements Chercheur
+public class menuAchat extends menuVente implements Chercheur
 {
     private Controleur controleur;
 
-    private critAchat Criteres;
     private Resultat<ProduitForm> Resultats;
 
     /**
@@ -35,69 +34,6 @@ public class menuAchat extends JPanel implements Chercheur
      */
     public menuAchat(Controleur c)
     {
-        super();
-        this.controleur = c;
-        initComponents();
+        super(c);
     }
-
-    /**
-     * Cette méthode est appelée depuis le constructeur pour initialiser le JPanel.
-     * ATTENTION : cette fonction reprend du code généré par un JForm.
-     */                       
-    private void initComponents()
-    {
-        this.setSize(500, 560);
-
-        this.Criteres = new critAchat(this.controleur, this);
-        this.Resultats = new Resultat<ProduitForm>(this);
-        
-        this.setLayout(new BorderLayout());
-        this.add(this.Criteres, BorderLayout.CENTER);
-        this.add(this.Resultats, BorderLayout.SOUTH);
-    }
-    
-    @Override
-    public void selectionnerResultat(Form res)
-    {
-        if (res instanceof ProduitForm) 
-            this.Criteres.setForm((ProduitForm) res);
-/*        else if (res instanceof FactureLigneForm)
-            this.Criteres.setForm(res);*/
-        else
-            throw new IllegalArgumentException("Erreur dans menuProduit: le formulaire à sélectionner n'est pas un ProduitForm ou un FactureLigneForm.");
-    }
-
-    @Override
-    public void lancerRecherche(Form form)
-    {
-        if (!(form instanceof CodeBarreForm))
-            throw new IllegalArgumentException("Erreur dans menuAchat: le formulaire à rechercher n'est pas un CodeBarreForm.");
-        try {
-            // Affectuer la recherche avec fonction RECHERCHE dans CONTROLEUR
-            Vector<ProduitForm> resultatsRecherche = null;
-            resultatsRecherche = this.controleur.chercher(form); //! TODO: attention, dans le contrôleur, à ce que renvoie chercherform()
-            // Afficher les résultats avec fonction AFFICHERES dans RESULTAT
-            if (resultatsRecherche != null)
-                this.Resultats.afficherRes(resultatsRecherche); }
-        catch (DonneeInvalideException e) {
-            afficherErreur(e);}
-        catch (controleur.DonneesInsuffisantesException e) {
-            afficherErreur(e);}
-        catch (ResultatInvalideException e) {
-            afficherErreur(e);}
-    }
-
-    @Override
-    public void afficherErreur(Exception e)
-    {
-        this.Resultats.afficherErreur(e.getMessage());
-    }
-
-    @Override
-    public void afficherLog(String log)
-    {
-        this.Resultats.afficherMessage(log);
-    }
-
-
 }
